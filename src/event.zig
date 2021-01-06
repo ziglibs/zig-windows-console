79d82897f5e2d1b20429946379ba88d7ebfa4d12
SuperAuguste <auguste.rame@gmail.com>
2021-01-06 18:57:39 -0500

    Add mouse event

--- src/event.zig
+++ src/event.zig
@@ -1,7 +1,14 @@
 const c = @import("c/c.zig");
 const vk = @import("c/vk.zig");
 const std = @import("std");
+
 const control_keys = @import("c/consts/control_keys.zig");
+const button_states = @import("c/consts/button_states.zig");
+
+pub const Coords = struct {
+    x: i16,
+    y: i16
+};
 
 pub const Key = union(enum) {
     ascii: u8,
@@ -43,8 +50,31 @@ pub const KeyEvent = struct {
     control_keys: ControlKeys
 };
 
-pub const MouseEvent = struct {
+pub const MouseButtons = struct {
+    const Self = @This();
 
+    left_mouse_button: bool = false,
+    middle_mouse_button: bool = false,
+    right_mouse_button: bool = false,
+
+    pub fn fromUnsigned(u: u32) Self {
+        var mb = Self{};
+        
+        inline for (std.meta.fields(Self)) |mb_field| {
+            if (u & @field(button_states, mb_field.name) == @field(button_states, mb_field.name))
+                @field(mb, mb_field.name) = true
+            else
+                @field(mb, mb_field.name) = false;
+        }
+
+        return mb;
+    }
+};
+
+pub const MouseEvent = struct {
+    abs_coords: Coords,
+    mouse_buttons: MouseButtons,
+    control_keys: ControlKeys
 };
 
 pub const Event = union(enum) {
@@ -69,7 +99,12 @@ pub const Event = union(enum) {
                 }};
             },
             c.MOUSE_EVENT => {
-                return Self{.mouse = .{}};
+                std.debug.print("{}\n", .{ir.Event.MouseEvent.dwButtonState});
+                return Self{.mouse =. {
+                    .abs_coords = @bitCast(Coords, ir.Event.MouseEvent.dwMousePosition),
+                    .mouse_buttons = MouseButtons.fromUnsigned(ir.Event.MouseEvent.dwButtonState),
+                    .control_keys = ControlKeys.fromUnsigned(ir.Event.MouseEvent.dwControlKeyState)
+                }};
             },
             else => std.debug.panic("Not implemented: {}!\n", .{ir.EventType})
         }
